04cded6bff13190e1144d889904574fff3b09c9e
itsjustchioma <74658446+itsjustchioma@users.noreply.github.com>
2022-09-06 00:57:20 +0100

    Update custom-page-component.js

--- Tutorial Notes/Scrimba/Tutorial Notes/custom-page-component.js
+++ Tutorial Notes/Scrimba/Tutorial Notes/custom-page-component.js
@@ -8,6 +8,16 @@ excited to be learning React :)
 
 Render your list to the page
 
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+
  */
  
  import React from "react";
@@ -16,13 +26,17 @@ Render your list to the page
  function LoveReact(){
   return(
   <div>
-   <h1>Reasons I Love React.</h1>
+   <nav>
+    <h1>Why I Love React</h1>
+    <img src="./react-logo.png" width="40px" />
+   </nav>
    <ol list-style-type="decimal">
      <li>I love React because of its versatility of flexibilty.</li>
      <li>I love React because of its reusable components.</li>
      <li>I love React because it makes me better at coding Javascript.</li>
      <li>React allows me work smarted not harder</li>
     </ol>
+   <footer>© 2022 Uche development. All rights reserved.</footer>
   </div>
    )
  };
