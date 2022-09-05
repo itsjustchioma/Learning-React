9cc88020e09d6cf0275824edf53d2fb8142c27ee
itsjustchioma <74658446+itsjustchioma@users.noreply.github.com>
2022-09-06 00:38:43 +0100

    Create custom-page-component.js

---
+++ Tutorial Notes/Scrimba/Tutorial Notes/custom-page-component.js
@@ -0,0 +1,17 @@
+/**
+Challenge: 
+
+Part 1: Create a page of your own using a custom Page component
+
+It should return an ordered list with the reasons why you're
+excited to be learning React :)
+
+Render your list to the page
+
+ */
+ 
+ import "React" from react;
+ import ReactDOM from "react/dom";
+ 
+ function LoveReact(){
+ }
