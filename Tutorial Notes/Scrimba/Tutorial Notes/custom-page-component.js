3d1a71e91e2740670754e47b9dfa054828b679ca
itsjustchioma <74658446+itsjustchioma@users.noreply.github.com>
2022-09-06 00:47:21 +0100

    Update custom-page-component.js

--- Tutorial Notes/Scrimba/Tutorial Notes/custom-page-component.js
+++ Tutorial Notes/Scrimba/Tutorial Notes/custom-page-component.js
@@ -14,4 +14,15 @@ Render your list to the page
  import ReactDOM from "react/dom";
  
  function LoveReact(){
- }
+  <div>
+   <h1>Reasons I Love React.</h1>
+   <ol list-style-type="decimal">
+     <li>I love React because of its versatility of flexibilty.</li>
+     <li>I love React because of its reusable components.</li>
+     <li>I love React because it makes me better at coding Javascript.</li>
+     <li>React allows me work smarted not harder</li>
+    </ol>
+  </div>
+ };
+
+ReactDOM.render(<LoveReact />, document.getElementById("root"));
