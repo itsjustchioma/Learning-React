4260f7a2707040e02009ad79e88190e246bc965b
itsjustchioma <74658446+itsjustchioma@users.noreply.github.com>
2022-09-11 00:34:56 +0100

    Add files via upload

---
+++ Tutorial Notes/Scrimba/Tutorial Notes/first-react-project/Appp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Components/nav-bar";
+import Main from "./Components/main";
+
+function Appp(){
+    return(
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
+export default Appp;
