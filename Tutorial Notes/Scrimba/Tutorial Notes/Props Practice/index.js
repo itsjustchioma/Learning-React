615f03706710d05ab01cbba437b0ceb270582dce
itsjustchioma <74658446+itsjustchioma@users.noreply.github.com>
2022-09-22 19:59:43 +0100

    Update index.js

--- Tutorial Notes/Scrimba/Tutorial Notes/Props Practice/index.js
+++ Tutorial Notes/Scrimba/Tutorial Notes/Props Practice/index.js
@@ -19,4 +19,10 @@ they always take things literally."
 
 If you don't pass in a "question" prop, how might you make it only 
 show the punchline?
-*/
+*/ 
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById('root'));
