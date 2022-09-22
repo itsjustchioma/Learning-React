73e3072d479d1337a3769c7fb8b2c8c8e86503ba
itsjustchioma <74658446+itsjustchioma@users.noreply.github.com>
2022-09-22 11:12:33 +0100

    Update App.js

--- Tutorial Notes/Scrimba/Tutorial Notes/Learning Props In React/App.js
+++ Tutorial Notes/Scrimba/Tutorial Notes/Learning Props In React/App.js
@@ -11,10 +11,30 @@ import Contact from "./Contact"
 function App() {
     return (
         <div className="contacts">
-            <Contact img="./images/mr-whiskerson.png" />
-            <Contact />
-            <Contact />
-            <Contact />
+            <Contact 
+                img="./images/mr-whiskerson.png" 
+                name="Mr Whiskerson"
+                number="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+            />
+            <Contact 
+                img="./images/fluffykins.png" 
+                name="Fluffykins"
+                number="(212) 555-2345"
+                email="fluff@me.com"
+            />
+            <Contact 
+                img="./images/felix.png" 
+                name="Felix"
+                number="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <Contact 
+                img="./images/pumpkin.png" 
+                name="Pumpkin"
+                number="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+            />
         </div>
     )
 }
