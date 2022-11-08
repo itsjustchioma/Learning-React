efe4be609ee9054804a9a2e58e5ce1f33e1ffdbb
Chioma Audrey Uche-Nwosu <74658446+itsjustchioma@users.noreply.github.com>
2022-11-08 21:37:08 +0100

    Update index.js

--- Tutorial Notes/Scrimba/Tutorial Notes/index.js
+++ Tutorial Notes/Scrimba/Tutorial Notes/index.js
@@ -1,3 +1,3 @@
 ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"));
 
-ReactDOM.render(<ul>Names of countries:<li>Nigeria</li><li>South-Africa</li></ul>, document.getElementById("list"))
+ReactDOM.render(<ul>Names of countries:<li>Nigeria</li><li>South-Africa</li></ul>, document.getElementById("list"));
